7d81b2c03754a1dcf39bbf0dae9d4c5036233270
Alexander Londoño Espejo <alexlondon07@gmail.com>
2017-11-06 15:15:14 -0500

    List view Example

--- app/src/main/java/io/github/alexlondon07/finalproject/model/Billboard.java
+++ app/src/main/java/io/github/alexlondon07/finalproject/model/Billboard.java
@@ -57,4 +57,75 @@ public class Billboard implements Serializable {
     private String IMDb;
 
 
+    public int getIdBollboard() {
+        return idBollboard;
+    }
+
+    public void setIdBollboard(int idBollboard) {
+        this.idBollboard = idBollboard;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getSinopsis() {
+        return sinopsis;
+    }
+
+    public void setSinopsis(String sinopsis) {
+        this.sinopsis = sinopsis;
+    }
+
+    public String getCast() {
+        return cast;
+    }
+
+    public void setCast(String cast) {
+        this.cast = cast;
+    }
+
+    public String getMovie_director() {
+        return movie_director;
+    }
+
+    public void setMovie_director(String movie_director) {
+        this.movie_director = movie_director;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public String getIMDb() {
+        return IMDb;
+    }
+
+    public void setIMDb(String IMDb) {
+        this.IMDb = IMDb;
+    }
 }
--- app/src/main/java/io/github/alexlondon07/finalproject/view/BillboardActivity.java
+++ app/src/main/java/io/github/alexlondon07/finalproject/view/BillboardActivity.java
@@ -1,15 +1,48 @@
 package io.github.alexlondon07.finalproject.view;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 import io.github.alexlondon07.finalproject.R;
+import io.github.alexlondon07.finalproject.model.Billboard;
 
 public class BillboardActivity extends AppCompatActivity {
+
+    private ListView listView;
+    private ArrayList<String> billboards;
+    private Billboard billboard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_billboard);
+
+        listView = findViewById(R.id.billboard_list_view);
+
+        //Data Example
+        billboards = new ArrayList<>();
+        billboards.add("6 DAYS");
+        billboards.add("PANDEMIA");
+        billboards.add("ATÓMICA");
+        billboards.add("SLEIGHT");
+        billboards.add("DESAPARECIDO");
+        billboards.add("TORRE OSCURA");
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(BillboardActivity.this, "Clicked " +billboards.get(position), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //My adapter Example
+        MyadapterExample myadapterExample = new MyadapterExample(this, R.layout.billboard_item, billboards);
+        listView.setAdapter(myadapterExample);
     }
 }
